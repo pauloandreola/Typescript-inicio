061c86fba0c95583538947a32900f7018b899b34
Paulo Andreola <pauloandreola@gmail.com>
2023-02-15 16:08:14 -0300

    feat: create routes to create mail method

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ import userController from "./controller/userController";
 const routes = Router();
 
 routes.get('/users', userController.index);
+routes.get('/users/create', userController.create);
 
 export default routes;
