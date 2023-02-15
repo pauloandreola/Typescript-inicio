e4853c9a2385efb4ae2bdd2ae9fa2f6b7b062e7c
Paulo Andreola <pauloandreola@gmail.com>
2023-02-15 20:08:29 -0300

    feat: frontend itial config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+interface IUser {
+  name: string;
+  email: string;
+}
+
+function App() {
+  const [ users, setUsers ] = useState<IUser[]>([]);
+  useEffect(() => {
+    api.get<IUser[]>('/users').then(response => {
+      setUsers(response.data);
+    })
+  }, [])
+  return (
+    <div className="App">
+      { users.map(user => <p>{user.name}</p>)}
+    
+    </div>
+  );
+}
+
+export default App;
