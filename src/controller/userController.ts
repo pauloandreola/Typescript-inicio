56a3ebba89ef0c651e41aa8062e0947c2449375e
Paulo Andreola <pauloandreola@gmail.com>
2023-02-15 14:02:05 -0300

    feat: mail service and controller method created

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/emailServices';
 
 const users = [{name: 'Paulo' , email: 'paulo@gmail.com'}];
 
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail(
+      {name: 'Paulo Andreola', email: 'paulo@gmail.com'},
+      {subject: 'Bem vindo ao sistema', body: 'Seja bem vindo ao typescript'}
+    );
+
+    return res.send();
   }
 };
