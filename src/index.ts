0f0caadddc00864122b3607a92ab9729c85e8212
Paulo Andreola <pauloandreola@gmail.com>
2023-02-15 00:46:34 -0300

    feat: instal e configure ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send('Hello world');
+  return res.send('Hello world 2');
 });
 
 app.listen(3333);
