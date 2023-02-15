be1446a1b4c2a46c256f85d0595d69d9f3a1b993
Paulo Andreola <pauloandreola@gmail.com>
2023-02-15 00:14:42 -0300

    feat: update import to run index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
+var express = require("express");
+var app = express();
 app.get('/', function (req, res) {
     return res.send('Hello world');
 });
