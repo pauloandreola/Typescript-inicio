03be515bc450342c726dbf933111b3f06f3ee737
Paulo Andreola <pauloandreola@gmail.com>
2023-02-15 21:15:07 -0300

    feat: finished frontend with typescript basic code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import api from './services/api';
 
+import User from './components/user';
+
 interface IUser {
   name: string;
   email: string;
@@ -12,10 +14,10 @@ function App() {
     api.get<IUser[]>('/users').then(response => {
       setUsers(response.data);
     })
-  }, [])
+  }, []);
   return (
     <div className="App">
-      { users.map(user => <p>{user.name}</p>)}
+      { users.map(user =><User key={user.email} user={user} />)}
     
     </div>
   );
